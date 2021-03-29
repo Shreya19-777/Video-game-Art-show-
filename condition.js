48b4818800168400a2b92ed768011e59143def99
s338641178 <79218718+s338641178@users.noreply.github.com>
2021-03-29 12:16:35 -0400

    Create condition.js

---
+++ condition.js
@@ -0,0 +1,25 @@
+function win() {
+  background(bg3);
+  fill(255);
+  textSize(40);
+  text('You won the game!', 80, 200);
+  text('You got 10 correct!', 80, 250);
+  textSize(20);
+  text('Reload page to play again!', 100, 300);
+}
+
+function gameOver() {
+  background(bg5);
+  textSize(30);
+  text("Incorrect!", 165, 120);
+  text('Your score was ' + score, 120, 220);
+  text('Correct answer is ' + answer + '!', 90, 170);
+  text('Reload page to play again', 70, 270);
+  console.log("Incorrect")
+}
+
+function afterGame() {
+  background(bg4);
+  textSize(30);
+  text('Game Over!' + '\n' + 'Reload page to play again', 80, 220);
+}
